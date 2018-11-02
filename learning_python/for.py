4234db546d27fb60f6e934979f8bfbb18cfbb933
Slava <kuldur196932@gmail.com>
2018-11-02 06:56:41 +0200

    added for into functions

--- learning_python/for.py
+++ learning_python/for.py
@@ -1,2 +1,23 @@
-for i in range(1, 10):
-    print(i)
+def for1():
+    print("Start for1")
+    for i in range(1, 10):
+        print(i)
+    print('Finish for1')
+
+def for2():
+    print('Start for2')
+    array=['A','B','C']
+    for a in array:
+        print (a)
+    print('Finish for2')
+
+def for3():
+    print('Start for3')
+    persones={'first_name':'Slava', 'lastname':'Starusev', 'age':50}
+    for persone in persones.values():
+        print(persone)
+    print('Finish for3')
+
+for1()
+for2()
+for3()
