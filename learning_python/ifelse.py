25e69a3a4059a9e8b2a673fb4163a37a4725e089
Slava <kuldur196932@gmail.com>
2018-11-02 10:00:52 +0200

    added elseif block

---
+++ learning_python/ifelse.py
@@ -0,0 +1,10 @@
+first_name = input('Enter your first name\n')
+# last_name=input('Enter your last name')
+if first_name == 'slava':
+    print('Your name is' +'======='+ first_name)
+
+elif first_name=='anna':
+    print('Your name is' + '=======' + first_name)
+
+else:
+    print('Enter correct name')
